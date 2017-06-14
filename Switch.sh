1faa8a383ff56192319a7c6454e4b24c3f052c1f
Prasad <prasad.bannuru@gmil.com>
2017-06-13 23:53:04 -0400

    Modified File Switch.sh

--- Switch.sh
+++ Switch.sh
@@ -3,9 +3,10 @@ echo "Please provide input1 and input2 values"
 #read A B
 A=$1
 B=$2
+Value=$3
 echo "1. Addition 2. Substraction 3. Multipliction 4. Division"
 echo "Enter the choice as displayed:"
-read Value
+#read Value
 case $Value in 
 1) Add=`expr $A + $B`
 	echo "Addition of $A and $B is:" $Add;;
